31053500f871408613a06f6be30c3c3d8086936e
aashu <sapkotasabin24@gmail.com>
2022-07-30 19:26:47 +0545

    added advanced authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,18 @@
+const crypto = require('crypto');
 const User = require("../models/user");
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: '',
+        pass: ''
+    },
+});
+
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -23,16 +36,16 @@ exports.getSignup = (req, res, next) => {
         message = null;
     }
     res.render('auth/signup', {
-        path:'/logout',
+        path: '/logout',
         pageTitle: 'Signup',
-        errorMessage : message
+        errorMessage: message
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    const email= req.body.email;
+    const email = req.body.email;
     const password = req.body.password;
-    User.findOne({email: email})
+    User.findOne({ email: email })
         .then(user => {
             if (!user) {
                 req.flash('error', 'Invalid email or password');
@@ -63,7 +76,7 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    User.findOne({email: email})
+    User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
                 req.flash('error', 'User with same email already exists!!');
@@ -71,18 +84,25 @@ exports.postSignup = (req, res, next) => {
             }
             return bcrypt
                 .hash(password, 12)
-                .then(hashedPassword => {const user = new User({
-                    email: email,
-                    password: hashedPassword,
-                    cart: {items: []}
-                });
-                return user.save();
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
                 })
                 .then(result => {
                     res.redirect('/login');
-                })
+                    return transporter.sendMail({
+                        to: '',
+                        from: '',
+                        subject: 'Signup Succeded',
+                        html: '<h1>You succesfully signed up!!</h1>'
+                    });
+                }).catch(err => console.log(err));
         })
-        .catch( err => console.log(err));
+        .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
@@ -91,3 +111,103 @@ exports.postLogout = (req, res, next) => {
         res.redirect('/');
     })
 };
+
+exports.getReset = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0]
+    } else {
+        message = null;
+    }
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        errorMessage: message
+    })
+}
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    req.flash('error', 'No account found with the requested email');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/');
+                transporter.sendMail({
+                    to: '',
+                    from: '',
+                    subject: 'Password Reset',
+                    html: `
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+                    `
+                });
+            })
+            .catch(err => console.log(err));
+    })
+}
+
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+        .then(user => {
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0]
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: '/new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+    User.findOne({
+        resetToken: passwordToken,
+        resetTokenExpiration: {$gt: Date.now()},
+        _id: userId
+    })
+    .then(user => {
+        if (!user) {
+            req.flash('error', 'User not found!!');
+            return res.redirect('/reset');               
+        }
+        resetUser = user;
+        return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashedPassword => {
+        resetUser.password = hashedPassword;
+        resetUser.resetToken = undefined;
+        resetUser.resetTokenExpiration = undefined;
+        return resetUser.save();
+    })
+    .then(result => {
+        res.redirect('/login');
+    })
+    .catch(err => console.log(err));
+}
